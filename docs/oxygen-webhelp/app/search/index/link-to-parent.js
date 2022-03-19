1850a462bec0b690eaf9600d0a7407b18c8cc1cc
Bobasy102 <92687559+Bobasy102@users.noreply.github.com>
2022-03-19 22:13:48 +0100

    Zmiany zadanie domowe DITA html
    Zmiany zadanie domowe DITA publikacja html

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {0:-1,2:8,5:-1,9:8,1:-1,11:8,13:8,7:8,3:8,12:8,8:1,4:8,6:8,10:8};
+return {6:8,10:8,13:8,4:8,3:8,8:1,11:8,1:-1,7:8,12:8,9:8,2:8};
 });
